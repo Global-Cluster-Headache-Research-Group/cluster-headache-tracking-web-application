0630e0905083afeb9a42f03429215d933ee2b01f
itka4yk <igor.tka4yk@gmail.com>
2021-03-31 21:10:19 +0200

    create attack endpoint

--- src/main/java/org/chtracker/dao/report/AttacksService.java
+++ src/main/java/org/chtracker/dao/report/AttacksService.java
@@ -1,20 +1,26 @@
 package org.chtracker.dao.report;
 
+import javassist.NotFoundException;
+import org.chtracker.dao.profile.Patient;
+import org.chtracker.dao.profile.PatientRepository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class AttacksService {
     private final AttackRepository attackRepository;
+    private final PatientRepository patientRepository;
 
-    public AttacksService(AttackRepository attackRepository) {
+    public AttacksService(AttackRepository attackRepository, PatientRepository patientRepository) {
         this.attackRepository = attackRepository;
+        this.patientRepository = patientRepository;
     }
 
     public Iterable<Attack> getAttacks(Pageable pageable, LocalDateTime from, LocalDateTime to) {
-        // TODO: add filterBy patient
+        // TODO: add filterBy patient from security context
         if (from == null && to == null) {
             return attackRepository.findAll(pageable);
         } else if (from == null) {
@@ -24,5 +30,12 @@ public class AttacksService {
         } else {
             return attackRepository.findAllByStartedBetween(from, to, pageable);
         }
+    }
+
+    public void addAttack(LocalDateTime started, LocalDateTime stopped, int patientId, int maxPainLevel, Boolean whileAsleep, String comments) throws NotFoundException {
+        Optional<Patient> patient = patientRepository.findById(patientId);
+        if (patient.isEmpty()) throw new NotFoundException(String.format("Patient with %n not found", patientId));
+        var attack = new Attack(started, stopped, patient.get(), maxPainLevel, whileAsleep, comments);
+        attackRepository.save(attack);
     }
 }
--- src/main/java/org/chtracker/web/AttackController.java
+++ src/main/java/org/chtracker/web/AttackController.java
@@ -1,16 +1,16 @@
 package org.chtracker.web;
 
+import javassist.NotFoundException;
 import org.chtracker.dao.report.Attack;
 import org.chtracker.dao.report.AttacksService;
+import org.chtracker.web.dto.AddAttackDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 
 @RestController
@@ -20,6 +20,11 @@ public class AttackController {
 
     public AttackController(AttacksService attacksService) {
         this.attacksService = attacksService;
+    }
+
+    @PostMapping
+    public void addAttack(@Valid @RequestBody AddAttackDto dto) throws NotFoundException {
+        this.attacksService.addAttack(dto.started, dto.stopped, dto.patientId, dto.maxPainLevel, dto.whileAsleep, dto.comments);
     }
 
     @GetMapping
